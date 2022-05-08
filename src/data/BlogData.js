c6d85dced88462d09b5c12cbb5f11302a0e845c1
Mayank kacha <87060138+MayankKacha@users.noreply.github.com>
2022-05-08 16:49:59 +0530

    First commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,113 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Working at an amazing company named Mystifly.",
+        tags:[".net-core","MVC","Full-stack_developer"],
+        date:"2nd May 2021 - present",
+        imgSrc:"https://www.breakingtravelnews.com/images/sized/images/uploads/airline/Mystifly-700x275.png",
+        link:"https://mystifly.com/"
+    },
+    {
+        id:2,
+        name:"MERN stack course - Coursera* (onGoing)",
+        tags:["HTML","CSS","Bootstrap","Javascript","React"],
+        date:"15 Dec, 2021 - present",
+        imgSrc:"https://about.coursera.org/static/whiteCoursera-23ec484f7091914430ce19b07d09aedf.svg",
+        link:"https://www.coursera.org/learn/front-end-react/home/welcome"
+    },
+    {
+        id:3,
+        name:"Learning DevOps* (onGoing)",
+        tags:["devops","ibm"],
+        date:"18 April, 2022 - present",
+        imgSrc:"https://about.coursera.org/static/whiteCoursera-23ec484f7091914430ce19b07d09aedf.svg",
+        link:"https://www.coursera.org/learn/intro-to-devops/home/welcome"
+    },
+    {
+        id:4,
+        name:"Certificates ? you ask! lets have it now. `Java programming`",
+        tags:["java","dsa","algorithms"],
+        date:"22 Aug, 2021",
+        imgSrc:"https://imageio.forbes.com/specials-images/imageserve/5e7cce1921695a000666cc29/Java-14/960x0.jpg?fit=bounds&format=jpg&width=960",
+        link:"https://www.hackerrank.com/certificates/90e34b090bb4"
+    },
+    {
+        id:5,
+        name:"One more certificate, `SQL`",
+        tags:["sql","database","dbms"],
+        date:"2 Oct, 2021",
+        imgSrc:"https://www.tutorialrepublic.com/lib/images/sql-illustration.png",
+        link:"https://www.hackerrank.com/certificates/faa60c62caab"
+    },
+    {
+        id:6,
+        name:"Last certificate, I promise. `Problem solving`",
+        tags:["DSA","algorithms","data structures"],
+        date:"22 Aug, 2021",
+        imgSrc:"https://miro.medium.com/max/1200/1*KpDOKMFAgDWaGTQHL0r70g.png",
+        link:"https://www.hackerrank.com/certificates/436804910d65"
+
+    },
+   
+    // {
+    //     id:7,
+    //     name:"What is Redux ? Simply Explained!",
+    //     tags:["react","redux","javascript"],
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+    // {
+    //     id:9,
+    //     name:"Certificates ? you ask! lets have it now. `Java programming`",
+    //     tags:["java","dsa","algorithms"],
+    //     date:"22 Aug, 2021",
+    //     imgSrc:"https://imageio.forbes.com/specials-images/imageserve/5e7cce1921695a000666cc29/Java-14/960x0.jpg?fit=bounds&format=jpg&width=960",
+    //     link:"https://www.hackerrank.com/certificates/90e34b090bb4"
+    // },
+    // {
+    //     id:10,
+    //     name:"One more certificate, `SQL`",
+    //     tags:["sql","database","dbms"],
+    //     date:"2 Oct, 2021",
+    //     imgSrc:"https://www.tutorialrepublic.com/lib/images/sql-illustration.png",
+    //     link:"https://www.hackerrank.com/certificates/faa60c62caab"
+    // },
+    // {
+    //     id:11,
+    //     name:"Last certificate, I promise. `Problem solving`",
+    //     tags:["DSA","algorithms","data structures"],
+    //     date:"22 Aug, 2021",
+    //     imgSrc:"https://miro.medium.com/max/1200/1*KpDOKMFAgDWaGTQHL0r70g.png",
+    //     link:"https://www.hackerrank.com/certificates/436804910d65"
+
+    // },
+   
+    // {
+    //     id:12,
+    //     name:"What is Redux ? Simply Explained!",
+    //     tags:["react","redux","javascript"],
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:13,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,84 @@
+// work data
+
+export const Work = [
+    // {
+    //     id:1,
+    //     name:"My portfolio website",
+    //     description:"Portfolio website built on top of React.js framework with HTML, CSS, Javascript. The website is responsive and works on all devices.",
+    //     tags:["react","html","css","javascript"],
+        
+    //     demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-job-listing-website"
+    // },
+    {
+       
+
+        id:1,
+        name:"Sorting visualizer",
+        description:"Whenever we start learning programming we come through this topic and I believe visualization of anything helps us to understand in much better way. So I have built `QuickSort sorting visualizer` with React.js, CSS and Javascript.",
+        tags:["react","javascript","css"],
+        
+        demo:"https://sorting-visualizer-7rw1enugi-mayankkacha.vercel.app/",
+        github:"https://github.com/MayankKacha/Sorting-visualizer"
+    },
+    {
+        id:2,
+        name:"Spotify Clone 1.0",
+        description:"A clone of Spotify build using ReactJS. With realtime fetching of User details and playlists from Spotify using API. It plays realtime music also if the user has a Spotify Premium account.",
+        tags:["react","api","css"],
+        
+        demo:"https://spotify-clone-five-mu.vercel.app/",
+        github:"https://github.com/MayankKacha/Spotify-clone"
+    },
+    {
+        id:3,
+        name:"Commander API",
+        description:"REST-API built using Dot net core and integrated with swagger. This api can be used to store,update,delete,search commands that we tend to forget in our everyday life.",
+        tags:["dotnet","restapi","swagger"],
+        
+        demo:"http://commanderapi.somee.com/index.html",
+        github:"https://github.com/MayankKacha/Commander-API"
+       
+    },{
+        id:4,
+        name:"Razor Pages Project",
+        description:"A simple razor page project with CRUD operations and in backend, MSSQL as DB and for styling and designing I used HTML,CSS and Javascript, to connect frontend with backend I used entity framework core.",
+        tags:["razorpages","crud","dotnet","entityframework"],
+        
+        demo:"http://razorproject.somee.com/",
+        github:"https://github.com/MayankKacha/RazorPageProject"
+        
+    },
+    {
+        id:5,
+        name:"Javascript project",
+        description:"A simple Javascript project in which I have included 5 small projects which gave me understanding of different areas where javascript can be used and how it can be used effectively. ",
+        tags:["javascript","rockpaperscissor","html","css"],
+        
+        demo:"https://mayankkacha.github.io/jsprojects.github.io/",
+        github:"https://github.com/MayankKacha/jsprojects.github.io"
+    },
+    {
+        id:6,
+        name:"Restaurant Page",
+        description:"It is simple fully handcoded web application where I used HTML,CSS,Javascript,Bootstrap 4.0",
+        tags:["bootstrap","html","css","javascript"],
+        
+        demo:"https://restaurant-cn8hbvw7c-mayankkacha.vercel.app/",
+        github:"https://github.com/MayankKacha/Visualstudio-Code"
+    },
+    {
+         
+        id:7,
+        name:"Library management application",
+        description:"Build with Dot net core with MVC framework and in backend, MSSQL as DB and for styling and designing I used HTML,CSS & Javascript,to connect frontend with backend I used entity framework core.",
+        tags:["dotnet","mvc","mssql","entityframework"],
+        
+        demo:"https://share.vidyard.com/watch/yiHRVM7TGQwbjH6n3ePtWa?",
+        github:"https://github.com/MayankKacha/LibraryManagement"
+        
+        
+    },
+    
+  
+] 
